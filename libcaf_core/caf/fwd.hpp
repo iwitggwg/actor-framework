b1b557d001d024ff8a4e840c8c2deb0bfafb21f1
Marian Triebe <m.triebe@live.de>
2016-03-22 09:38:35 +0100

    Add replication submodule

--- libcaf_core/caf/fwd.hpp
+++ libcaf_core/caf/fwd.hpp
@@ -125,6 +125,14 @@ class manager;
 
 } // namespace opencl
 
+// -- Replication classes ------------------------------------------------------
+
+namespace replication {
+
+class replicator;
+
+} // namespace replication
+
 // -- RIAC classes -------------------------------------------------------------
 
 namespace riac {
